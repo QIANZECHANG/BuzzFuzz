468b058a07c711e5e778c1a39bfa1beabeea29bb
QIANZECHANG <qianzechang@gmail.com>
2020-06-18 16:30:54 +0200

    libdft+ins

--- libdft/BuzzFuzz.cpp
+++ libdft/BuzzFuzz.cpp
@@ -24,6 +24,7 @@
 #include "tagmap.h"
 
 extern syscall_desc_t syscall_desc[SYSCALL_MAX];
+extern ins_desc_t ins_desc[XED_ICLASS_LAST];
 
 void
 alert(uintptr_t addr, uint8_t tag)
@@ -41,16 +42,17 @@ post_read_hook(syscall_ctx_t *ctx)
   void *buf  =  (void*)ctx->arg[SYSCALL_ARG1];
   size_t len = (size_t)ctx->ret;
   uint8_t color=0x01;
-
+ 
   if(unlikely(len <= 0)) {
     return;
   }
 
-  printf("(BuzzFuzz) read: %zu bytes from fd %u\n", len, fd);
+  printf("(BuzzFuzz_read) read: %zu bytes from fd %u\n", len, fd);
   printf("(BuzzFuzz_read) tainting bytes %p -- 0x%x with color 0x%x\n", 
           buf, (uintptr_t)buf+len, color);
 
   tagmap_setn((uintptr_t)buf, len, color); 
+  
 }
 /*
 32bitでmmapがmmap２となる
@@ -64,8 +66,7 @@ post_mmap2_hook(syscall_ctx_t *ctx){
   void *len_addr=&ctx->arg[SYSCALL_ARG1];
   uint8_t color;
 
-  printf("map_len_addr : %p\n",len_addr);
-
+  printf("(mmap2) len_addr : %p\n",len_addr);
   color = tagmap_getb((uintptr_t)len_addr);
 
   if(color){
@@ -73,6 +74,37 @@ post_mmap2_hook(syscall_ctx_t *ctx){
   }else{
     return;
   }
+}
+
+static void
+taint_eax(uint32_t reg){
+        uint8_t color=0x01;
+        void* addr=&reg;
+        
+        if(reg==0x3)
+        printf("(taint_eax) %p\n",&reg);
+	
+        tagmap_setn((uintptr_t)addr,1,color);
+}
+
+
+static void
+dta_eax(INS ins,void* v)
+{
+	IMG img = IMG_FindByAddress(INS_Address(ins));
+        if(!IMG_Valid(img)||!IMG_IsMainExecutable(img))return;
+
+        REG reg;
+
+        if (INS_OperandIsReg(ins, 0)) {
+         	reg = INS_OperandReg(ins, 0);
+                
+		INS_InsertCall(ins,
+			IPOINT_BEFORE,
+                        (AFUNPTR)taint_eax,
+			IARG_REG_VALUE, reg,
+			IARG_END);
+	}
 }
 
 int
@@ -88,12 +120,14 @@ main(int argc, char **argv)
     libdft_die();
     return 1;
   }
-
+  
   syscall_set_post(&syscall_desc[__NR_read], post_read_hook);
   syscall_set_pre(&syscall_desc[__NR_mmap2], post_mmap2_hook);
-
+  //ins_set_pre(&ins_desc[XED_ICLASS_MOVZX],dta_eax);
+  INS_AddInstrumentFunction(dta_eax,NULL);
+ 
   PIN_StartProgram();
-	
+ 
   return 0;
 }
 
--- program/test.c
+++ program/test.c
@@ -1,7 +1,3 @@
-/*
-jpegファイルからimageのwidthとheight情報を保存するバイトを検出して（簡単のため、こちらでwidthだけを注目）、
-width分の空間をmmap関数で作成し、そこに'aaaaaaaaaa'を書き込む。
-*/
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -15,7 +11,6 @@ int main(int argc,char* argv[]){
     
     int fd = open(argv[1],O_RDONLY); 
     
-    //入力するjpegファイルのバイト数が16,000ぐらいので、簡単にするためにsizeが4096*5であるbufを定義
     unsigned char buf[4096*5];
     int i;
     char* text;
@@ -24,16 +19,17 @@ int main(int argc,char* argv[]){
     int len;
     len=read(fd,buf,4096*5);
     for(i=0;i<len;i++){
-        if(buf[i]==0xff&&buf[i+1]==0xc0){ // jpegファイルフォーマットにより、widthとheightを検出
+        if(buf[i]==0xff&&buf[i+1]==0xc0){ 
             //height=buf[i+5]*256+buf[i+6];
             //width=buf[i+7]*256+buf[i+8];
-            
-            //かけ算を回避するために、widthの一つのバイトを入力するだけ
             text=mmap(NULL,buf[i+8],PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANON,-1,0);
             printf("i+7 : %p\ni+8 : %p\n",&buf[i+7],&buf[i+8]);
             break;
         } 
     }
+
+    //text=mmap(NULL,c,PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANON,-1,0);
+   
     memset(text,'a',10);
     printf("%s\n",text);
     close(fd);
